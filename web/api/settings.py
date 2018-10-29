0ad4382cf338f03874e30586b612c66e9a8eacb6
renciweb <mwatson@renci.org>
2018-10-29 11:27:15 -0400

    begin frontend container setup

--- web/api/settings.py
+++ web/api/settings.py
@@ -45,12 +45,22 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+# CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ALLOW_CREDENTIALS = True
+# CCORS_ORIGIN_WHITELIST = (
+#     'localhost:80',
+# )
+# CORS_ORIGIN_REGEX_WHITELIST = (
+#     'localhost:80',
+# )
 
 ROOT_URLCONF = 'api.urls'
 
