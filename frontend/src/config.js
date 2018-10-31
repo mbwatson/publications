7035e19fbd458f7053d1dc76f55149522160a029
renciweb <mwatson@renci.org>
2018-10-31 11:22:43 -0400

    add api url env var

--- frontend/src/config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-    // apiUrl: 'http://localhost:8000/api/publications/',
-    apiUrl: 'http://localhost:8000/api/publications',
-    apiKey: 'apikeyapikeyapikeyapikeyapikeyapikeyapikey',
-}
-
-export default config
